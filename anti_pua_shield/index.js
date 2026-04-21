573183c13a62b6483a0df0cde24bcb881319fb4a
DESKTOP-EF928QT\15392 <2394123911@qq.com>
2026-04-21 17:11:26 +0800

    修改：允许跨域

--- anti_pua_shield/index.js
+++ anti_pua_shield/index.js
@@ -10,7 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // 中间件
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
